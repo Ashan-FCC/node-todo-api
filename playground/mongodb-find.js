950d14865f23d44fc2a450d04a38d701b1599666
Ashan Perera <ashan.navin@gmail.com>
2016-11-22 12:04:47 +0700

    Make a few DB calls

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -7,8 +7,10 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db)=> {
 					}
 					console.log('Connected to database');
 
-					var collection = db.collection('Todos').find({
-						_id: new ObjectID("583285f11f5978243feeb1d5")
+					var collection = db.collection('Users');
+
+					collection.find({
+						name: "Ashan Perera"
 					}).toArray().then((docs) => {
 						console.log('Todos');
 						console.log(JSON.stringify(docs, undefined, 2));
@@ -16,6 +18,11 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db)=> {
 						console.log('Unable to fetch todso',err);
 					});
 
+					// collection.find().count().then((count)=>{
+					// 	console.log(`Todos count: ${count}`);
+					// }, (err) => {
+					// 	console.log('Unable to fetch todos');
+					// })
 
 					//db.close();
 				});
